ab4f79ed964bb1d53a3acf943c3bf0a8632a0ef6
罗赞 <iluozan@126.com>
2015-12-12 20:54:03 +0800

    上传新项目

---
+++ vaccine-factory/data/gameData.js
@@ -0,0 +1,18 @@
+var $data = {
+	money:[
+		{id:'salesman',name:'推销员',price:500,initMAX:1000,increment:1000,speed:2,number:0}
+	],
+	vaccine:[
+		{id:'worker',name:'制药工人',price:1000,initMAX:5,increment:5,speed:1}
+	],
+	list:[
+		[
+		],
+		[
+		],
+		[
+		],
+		[
+		]
+	]
+};
